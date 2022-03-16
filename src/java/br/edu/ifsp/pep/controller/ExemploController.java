77fc3f5c277eb00344c2f995a1432dd3ad3b8934
aluno <aluno@LAB19M10>
2022-03-16 10:32:14 -0300

    Progresso no adicionar() do controller Layout para adicionar "funcionando"

--- src/java/br/edu/ifsp/pep/controller/ExemploController.java
+++ src/java/br/edu/ifsp/pep/controller/ExemploController.java
@@ -7,6 +7,7 @@ package br.edu.ifsp.pep.controller;
 import br.edu.ifsp.pep.dao.PessoaDAO;
 import br.edu.ifsp.pep.dao.ProdutoDAO;
 import br.edu.ifsp.pep.model.Pessoa;
+import br.edu.ifsp.pep.model.Produto;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -24,13 +25,17 @@ import javax.inject.Named;
 @Named
 //@RequestScoped
 @ViewScoped
-public class ExemploController implements Serializable{
+public class ExemploController implements Serializable {
 
     @Inject
     private PessoaDAO pessoaDAO;
-    
+
     @Inject
     private ProdutoDAO produtoDAO;
+
+    private Produto produto;
+    private List<Produto> produtos;
+    private Produto produtoSelecionado;
 
     private Pessoa pessoa;
     private List<Pessoa> pessoas;
@@ -44,8 +49,6 @@ public class ExemploController implements Serializable{
         this.pessoaSelecionada = pessoaSelecionada;
     }
 
-
-
     public Pessoa getPessoa() {
         return pessoa;
     }
@@ -56,62 +59,99 @@ public class ExemploController implements Serializable{
 
     public List<Pessoa> getPessoas() {
 //        return pessoas;
-        if(this.pessoas == null){
-           System.out.println("Carregando...");
-           this.pessoas = pessoaDAO.findAll();
+        if (this.pessoas == null) {
+            System.out.println("Carregando...");
+            this.pessoas = pessoaDAO.findAll();
         }
 
         return pessoaDAO.findAll();
- 
+
     }
+
+    public ProdutoDAO getProdutoDAO() {
+        return produtoDAO;
+    }
+
+    public void setProdutoDAO(ProdutoDAO produtoDAO) {
+        this.produtoDAO = produtoDAO;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
+    }
+
+    public Produto getProdutoSelecionado() {
+        return produtoSelecionado;
+    }
+
+    public void setProdutoSelecionado(Produto produtoSelecionado) {
+        this.produtoSelecionado = produtoSelecionado;
+    }
+
 //
 //    public void setPessoas(List<Pessoa> pessoas) {
 //        this.pessoas = pessoas;
 //    }
+    public void excluir() {
 
-    public void excluir(){
-        
         if (pessoaSelecionada != null) {
             System.out.println("Metodo Excluir");
             pessoaDAO.remove(pessoaSelecionada);
             addMessage(FacesMessage.SEVERITY_INFO, "Informação", "Pessoa excluida");
             this.pessoas = null;
-        }else{
+        } else {
             addMessage(FacesMessage.SEVERITY_ERROR, "Erro", "Pessoa Não selecionada");
             return;
         }
 
-        
     }
 
+    public void adicionar() {
+        
+//        System.out.println("Nome: " + this.pessoa.getNome());
+//        pessoaDAO.inserir(pessoa);
+//        this.pessoas.add(pessoa);
+//        //this.pessoas = null; // Metodo mais lento
+//        this.pessoa = new Pessoa();
+//
 
-    public void adicionar(){
 
         System.out.println("Executou o método adicionar");
-        System.out.println("Nome: " + this.pessoa.getNome());
-        pessoaDAO.inserir(pessoa);
-        this.pessoas.add(pessoa);
-        //this.pessoas = null; // Metodo mais lento
-        this.pessoa = new Pessoa();
-
-        addMessage(FacesMessage.SEVERITY_INFO, "Informação", "Cadastro Realizado");             
+        System.out.println("Produto");
+        System.out.println(produto);
+        //this.produtos.add(produto);
+        
+        produtoDAO.inserir(produto);
+        
+        this.produto = new Produto();
+        addMessage(FacesMessage.SEVERITY_INFO, "Informação", "Cadastro Realizado");
 
     }
 
-    public void exibir(){
+    public void exibir() {
         System.out.println("Executou o método exibir");
 //        for (Pessoa p : pessoas) {
 //            System.out.println("Nome na lista: " + p.getNome());
 //        }
     }
 
+    public ExemploController() {
 
-
-    public ExemploController(){
-
-        System.out.println("Construido");   
+        System.out.println("Construido");
         this.pessoa = new Pessoa();
- 
+        this.produto = new Produto();
 
     }
 
@@ -120,7 +160,4 @@ public class ExemploController implements Serializable{
                 addMessage(null, new FacesMessage(severity, summary, detail));
     }
 
-    
-    
-    
 }
